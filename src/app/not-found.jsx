b53ed8c3a48f29a4b6bf86c526f4751263ff2ccb
jamal-uddin-asif <asifzehendmg@gmail.com>
2026-08-29 10:16:50 +0600

    Initial commit from Create Next App

---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const not-found = () => {
+    return (
+        <div>
+            <h2>Page not found 404</h2>
+        </div>
+    );
+};
+
+export default not-found;
