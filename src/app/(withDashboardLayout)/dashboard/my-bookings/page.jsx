dd5e8d2ccc8d5f1227f9ee4641c56893df976a9b
jamal-uddin-asif <asifzehendmg@gmail.com>
2026-09-02 19:35:58 +0600

    My bookings page content added

--- src/app/(withDashboardLayout)/dashboard/my-bookings/page.jsx
+++ src/app/(withDashboardLayout)/dashboard/my-bookings/page.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+"use client";
+import ServiceCard from "@/app/(withCommonLayout)/services/_components/ServiceCard";
+import { BookingContext } from "@/context/booking.context";
+import React, { useContext } from "react";
 
-const myBookings = () => {
-    return (
-        <div>
-            My bookings here
-        </div>
-    );
+const MyBookings = () => {
+  const { bookings } = useContext(BookingContext);
+  return (
+    <div>
+      <div className="m-3">
+        {bookings.length === 0 ? (
+          <h2 className="text-center text-3xl text-purple-600">
+            Bookings not found{" "}
+          </h2>
+        ) : (
+          <div className="grid my-5 grid-cols-3 gap-4">
+            {bookings?.map((service) => (
+              <ServiceCard key={service?._id} service={service} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
-export default myBookings;
+export default MyBookings;
